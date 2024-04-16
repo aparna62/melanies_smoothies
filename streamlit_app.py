ed4ba5283c2cacdeda2d9777bfce4695e245dfa8
aparna62 <106722318+aparna62@users.noreply.github.com>
2024-04-16 16:21:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 cnx=st.connection("Snowflake")
 session=cnx.session()
@@ -23,6 +24,10 @@ if ingredients_list:
     ingredients_string=''
     for fruit_choosen in ingredients_list:
         ingredients_string+=fruit_choosen+' '
+        st.subheader(fruit_chosen+'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choosen")
+        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+        #st.text(fruityvice_response.json())
     st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
@@ -33,8 +38,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
+
+
+
 
